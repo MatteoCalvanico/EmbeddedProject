dee2569724f874821b72b0beb1991d1d27488839
matteo calvanico <matteo.calvanico03@gmail.com>
2024-02-14 16:17:19 +0100

    Add WiFi part

--- ESP32/sketch.ino
+++ ESP32/sketch.ino
@@ -1,8 +1,25 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
 #define GreenLed 4 //Led per indicare che il parcheggio è libero
 #define RedLed 2   //Led per indicare che il parcheggio è occupato
 
 #define TrigPin 18 //Collegamento fra ESP32 e sensore ad Ultrasuoni (TRIG)
 #define EchoPin 5  //Collegamento fra ESP32 e sensore ad Ultrasuoni (ECHO)
+
+
+//Wi-Fi part
+const char* ssid = "Wokwi-GUEST";
+const char* psw = "";
+
+//MQTT part
+const char* mqttServer = "broker.emqx.io";
+const int mqttPort = 1883;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 
 //Controllo se c'è un'auto parcheggiata (Rileva un oggetto a meno di 50 cm)
@@ -31,6 +48,18 @@ bool isParked() {
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, psw);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Try to connect...");
+    delay(400);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqttServer, mqttPort);
+
   pinMode(GreenLed, OUTPUT);
   pinMode(RedLed, OUTPUT);
   pinMode(TrigPin, OUTPUT);
@@ -44,16 +73,19 @@ void setup() {
 }
 
 void loop() {
-  if(isParked()){
-    digitalWrite(GreenLed, LOW);
-    digitalWrite(RedLed, HIGH);
-    Serial.println(("!!! Parcheggio occupato !!!"));
-  }else{
-    digitalWrite(GreenLed, HIGH);
-    digitalWrite(RedLed, LOW);
-    Serial.println(("!!! Parcheggio libero !!!"));
+  if(!client.connected()){
+    if(isParked()){
+      digitalWrite(GreenLed, LOW);
+      digitalWrite(RedLed, HIGH);
+      Serial.println(("!!! Parcheggio occupato !!!"));
+    }else{
+      digitalWrite(GreenLed, HIGH);
+      digitalWrite(RedLed, LOW);
+      Serial.println(("!!! Parcheggio libero !!!"));
+    }
   }
 
 
-  delay(1000);
+
+  delay(500);
 }
