45c83cd0dc81aa3bef87e186b902afb29d9e9e33
matteo calvanico <matteo.calvanico03@gmail.com>
2024-02-15 14:43:56 +0100

    Add MQTT part

--- ESP32/sketch.ino
+++ ESP32/sketch.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
 
@@ -16,10 +17,21 @@ const char* psw = "";
 //MQTT part
 const char* mqttServer = "broker.emqx.io";
 const int mqttPort = 1883;
+const char* mqttTopic = "topic";
+const char* mqttUsername = "parcheggio1";
+const char* mqttPassword = "";
+
+//parametri per evitare blocchi
+unsigned long previousMillis = 0;
+const long interval = 1000;
 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
 
 
 //Controllo se c'è un'auto parcheggiata (Rileva un oggetto a meno di 50 cm)
@@ -46,19 +58,63 @@ bool isParked() {
   }
 }
 
+//Connessione al Wi-Fi
+void WIFIconnect(){
+  WiFi.begin(ssid, psw);
+  Serial.println("Connessione...");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(1000);
+  }
+  Serial.println("Connessione riuscita");
+  Serial.println("indirizzo IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+//Connessione MQTT
+void MQTTconnect(){
+  while (!client.connected()) {
+    Serial.println("Connessione a MQTT…");
+    String clientId = "ESP32Client-"; //Creazione ID client random
+    clientId += String(random(0xffff), HEX);
+    
+    //Connessione
+    if (client.connect(clientId.c_str(), mqttUsername, mqttPassword)) {
+      Serial.println("Connesso");
+
+      client.subscribe(mqttTopic);
+    } else {
+      Serial.print("FAIL, rc=");
+      Serial.print(client.state());
+      Serial.println("Ri-connessione fra 3 secondi");
+      delay(3000);
+    }
+  }
+}
+
+//In caso di arrivo di messaggi dal broker
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) {
+    incommingMessage+=(char)payload[i];
+  }
+  Serial.println("Messaggio da MQTT ["+String(topic)+"] " + incommingMessage);
+}
+
 void setup() {
   Serial.begin(115200);
 
-  WiFi.begin(ssid, psw);
-  while(WiFi.status() != WL_CONNECTED) {
-    Serial.println("Try to connect...");
-    delay(400);
-  }
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
+  //START Wi-Fi connection
+  WIFIconnect();
+  //END Wi-Fi connection
+
+
+  //START MQTT connection
   client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  //END MQTT connection
+
 
   pinMode(GreenLed, OUTPUT);
   pinMode(RedLed, OUTPUT);
@@ -67,25 +123,33 @@ void setup() {
 
 
   Serial.println("!!! Inizializzazione parcheggio !!!");
-
   digitalWrite(GreenLed, HIGH);
-  Serial.println(("!!! Parcheggio libero !!!"));
+  Serial.println("!!! Parcheggio libero !!!");
 }
 
 void loop() {
   if(!client.connected()){
+    MQTTconnect();
+  }
+  client.loop();
+
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
     if(isParked()){
       digitalWrite(GreenLed, LOW);
       digitalWrite(RedLed, HIGH);
-      Serial.println(("!!! Parcheggio occupato !!!"));
+      Serial.println("!!! Parcheggio occupato !!!");
+
+      Serial.println("Invio dati al server...");
+      client.publish(mqttTopic, "true"); //Indico al server che il parcheggio è occupato
     }else{
       digitalWrite(GreenLed, HIGH);
       digitalWrite(RedLed, LOW);
-      Serial.println(("!!! Parcheggio libero !!!"));
+      Serial.println("!!! Parcheggio libero !!!");
+
+      Serial.println("Invio dati al server...");
+      client.publish(mqttTopic, "false"); //Indico al server che il parcheggio è libero
     }
   }
-
-
-
-  delay(500);
 }
