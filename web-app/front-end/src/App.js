9860fedbeca900c9b753306ef74baace513c8f38
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-02-20 17:03:33 +0100

    polling fetch component

--- web-app/front-end/src/App.js
+++ web-app/front-end/src/App.js
@@ -1,30 +1,13 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import data from "./subscriber/output.csv";
+import React from "react";
+import DataComponent from "./components/DataComponent";
 
 function App() {
-  useEffect(() => {
-    fetch("./subscriber/output.csv")
-      .then((response) => response.text())
-      .then((text) => {
-        // Split the text by line breaks to get individual rows
-        const rows = text.split("\n");
-
-        // Skip the first row if it contains headers
-        rows.shift();
-
-        // Process each row, assuming it's comma-separated
-        rows.forEach((row) => {
-          const values = row.split(",");
-          // Process the values here, e.g., log them to the console
-          console.log(values);
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching or parsing CSV:", error);
-      });
-  }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DataComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ web-app/front-end/src/components/DataComponent.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+const DataComponent = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setData(null);
+      try {
+        const response = await fetch("http://localhost:7000/");
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const intervalId = setInterval(fetchData, 10000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <p>Data from server: {JSON.stringify(data)}</p>
+      ) : (
+        <p>Fetching data...</p>
+      )}
+    </div>
+  );
+};
+
+export default DataComponent;
